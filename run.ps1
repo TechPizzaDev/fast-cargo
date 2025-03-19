ecb7c26423dc31f2b088d377dbbd243eb50906e7
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-03-19 16:28:11 +0100

    V1 of testing script

---
+++ run.ps1
@@ -0,0 +1,57 @@
+
+$SourceDir = "./crates/bevy-hello-world"
+$BuildDir = "./build"
+
+$WorkDir = Join-Path $BuildDir $SourceDir
+
+Remove-Item $WorkDir -Recurse -Force
+Copy-Item -Path $SourceDir -Destination $WorkDir -Recurse
+
+Push-Location $WorkDir
+
+# We need to use nightly toolchain for advanced and experimental options (primarily "-Z").
+$Env:RUSTUP_TOOLCHAIN = "nightly"
+
+# Incremental compile slows down initial compile, and should be benched separately. 
+# Incremental primarily uses 256 codegen units to function, which inhibits link-time opts.
+$Env:CARGO_INCREMENTAL = 0
+
+cargo -V
+
+$CurDir = (Get-Location).Path
+
+# Build statically linked exe.
+# This links all dependencies, including the Rust libstd. 
+$Env:RUSTFLAGS="
+    -Zself-profile=$($CurDir)/profiles-static 
+    -Zself-profile-events=default
+    "
+    
+cargo build `
+    -Z unstable-options `
+    --artifact-dir="$($CurDir)/out-static" `
+
+cargo run
+
+# Build dynamically linked exe.
+# Rust libstd will use the dylib from toolchain.
+# Internal bevy crates will be linked into one big dylib.
+$Env:RUSTFLAGS="
+    -Z self-profile=$($CurDir)/profiles-dylib
+    -Z self-profile-events=default
+
+    -C strip=symbols
+    -C split-debuginfo=off
+    -C link-arg=--ld-path=/usr/bin/mold
+    -C linker=clang
+    -C prefer-dynamic=yes
+    "
+
+cargo build `
+    --features bevy/dynamic_linking `
+    -Z unstable-options `
+    --artifact-dir="$($CurDir)/out-dylib"
+
+cargo run `
+    --features bevy/dynamic_linking `
+    -Z unstable-options
